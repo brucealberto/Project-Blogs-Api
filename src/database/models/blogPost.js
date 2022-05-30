7eb33d1ceb4783ba2a9c29c71d1e5411d6963ccd
Bruce <brucealberto2@gmail.com>
2022-05-30 19:19:56 -0300

    Requisito 10

---
+++ src/database/models/blogPost.js
@@ -0,0 +1,22 @@
+const BlogPost = (sequelize, DataTypes) => {
+  const BlogPost = sequelize.define(
+    "BlogPost",
+    {
+      id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+      title: DataTypes.STRING,
+      content: DataTypes.STRING,
+      userId: DataTypes.INTEGER,
+      published: DataTypes.STRING,
+      updated: DataTypes.STRING,
+    },
+    { timestamps: false }
+  );
+
+  BlogPost.associate = (models) => {
+    BlogPost.belongsTo(models.User, { as: "user", foreignKey: "userId" });
+  };
+
+  return BlogPost;
+};
+
+module.exports = BlogPost;
