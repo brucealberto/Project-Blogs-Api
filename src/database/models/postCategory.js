5f08566461cec39475b43f15fe803e05104241f2
Bruce <brucealberto2@gmail.com>
2022-05-31 16:16:51 -0300

    Requisito 11

---
+++ src/database/models/postCategory.js
@@ -0,0 +1,27 @@
+const PostCategory = (sequelize, DataTypes) => {
+  const PostCategory = sequelize.define(
+    "PostCategory",
+    { postId: DataTypes.INTEGER, categoryId: DataTypes.INTEGER },
+    { timestamps: false }
+  );
+
+  PostCategory.associate = (models) => {
+    models.BlogPost.belongsToMany(models.Category, {
+      as: "category",
+      through: PostCategory,
+      foreignKey: "postId",
+      otherKey: "categoryId",
+    });
+
+    models.Category.belongsToMany(models.BlogPost, {
+      as: "blogpost",
+      through: PostCategory,
+      foreignKey: "categoryId",
+      otherKey: "postId",
+    });
+  };
+
+  return PostCategory;
+};
+
+module.exports = PostCategory;
